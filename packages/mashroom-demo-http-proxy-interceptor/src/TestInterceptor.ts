827ac1d88f0651702bb7ecdb900ec6865734614a
Jürgen Kofler <juergen.kofler@nonblocking.at>
2024-01-28 13:36:55 +0100

    Upgrade to Mashroom 2.5.4

--- packages/mashroom-demo-http-proxy-interceptor/src/TestInterceptor.ts
+++ packages/mashroom-demo-http-proxy-interceptor/src/TestInterceptor.ts
@@ -1,12 +1,12 @@
 
 import type {Request, Response} from 'express';
 import type {IncomingMessage} from 'http';
-import type {MashroomHttpProxyInterceptor, HttpHeaders, QueryParams} from '@mashroom/mashroom-http-proxy/type-definitions';
+import type {MashroomHttpProxyInterceptor, HttpHeaders, QueryParams, MashroomHttpProxyRequestInterceptorResult} from '@mashroom/mashroom-http-proxy/type-definitions';
 
 export default class TestInterceptor implements MashroomHttpProxyInterceptor {
 
     async interceptRequest(targetUri: string, existingHeaders: Readonly<HttpHeaders>, existingQueryParams: Readonly<QueryParams>,
-                           clientRequest: Request, clientResponse: Response) {
+                           clientRequest: Request, clientResponse: Response): Promise<MashroomHttpProxyRequestInterceptorResult | undefined | null> {
         const logger = clientRequest.pluginContext.loggerFactory('test.http.interceptor');
         const securityService = clientRequest.pluginContext.services.security && clientRequest.pluginContext.services.security.service;
 
@@ -15,7 +15,12 @@ export default class TestInterceptor implements MashroomHttpProxyInterceptor {
         logger.info(`===== Intercepting http proxy request: ${targetUri}, user: ${user.username} =====`);
 
 
-        return null;
+        return {
+            addHeaders: {
+                'x-test': '1',
+                'invalid-header': undefined,
+            },
+        };
     }
 
     async interceptResponse(targetUri: string, existingHeaders: Readonly<HttpHeaders>, targetResponse: IncomingMessage,
